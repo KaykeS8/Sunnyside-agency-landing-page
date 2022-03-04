3060568f475436bcd782e2fd397e069395f16b03
kayke <kayke.py@gmail.com>
2022-03-03 21:14:28 -0300

    Arquivo com o codigo da funcionalidade do menu mobile

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ const btnMobile = document.querySelector('[data-menu="menu-open"]');
 const burguer = document.querySelector('.burger');
 const nav = document.querySelector('.nav');
 
-console.log(nav)
 
 btnMobile.addEventListener('click', () => {
     burguer.classList.toggle('close')
